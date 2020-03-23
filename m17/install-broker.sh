2092c9d4c236fa798944efbb441d08a09f115ec7
vlele <vishwas.lele@appliedis.com>
2020-03-23 14:16:24 +0530

    Bug Fix

--- m17/install-broker.sh
+++ m17/install-broker.sh
@@ -6,4 +6,5 @@ kubectl config use-context $(kubectl config current-context)
 helm fetch --untar azure/open-service-broker-azure
 sed -i 's|extensions/v1beta1|apps/v1|g' ./open-service-broker-azure/templates/deployment.yaml
 sed -i 's|extensions/v1beta1|apps/v1|g' ./open-service-broker-azure/charts/redis/templates/deployment.yaml
+sed -i 's|  template:|  selector:\n    matchLabels:\n      app: {{ template "redis.fullname" . }}\n  template: |g' ./open-service-broker-azure/charts/redis/templates/deployment.yaml
 helm install ./open-service-broker-azure/ --name osba --namespace osba --set azure.subscriptionId=$AZURE_SUBSCRIPTION_ID --set azure.tenantId=$AZURE_TENANT_ID --set azure.clientId=$AZURE_CLIENT_ID --set azure.clientSecret=$AZURE_CLIENT_SECRET
